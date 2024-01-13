c8b3b714e16814b139a81aa054a79620a81c8fa3
amschel99 <6290.2020@students.ku.ac.ke>
2024-01-13 12:08:58 +0300

    crete onboarding functions

--- backend/types.ts
+++ backend/types.ts
@@ -7,45 +7,29 @@ import {
   nat,
   nat64,
   float32,
+  int8,
+  blob
+
 } from "azle";
 
 
 
+interface User {
+  internetId:text
+}
+export interface Company extends User {
+  id:text,
+  companyName:text,
+  description:text,
+  publicShares:float32,
+  legalDoc:blob,
+  businessInfoDoc:blob
 
-export type User = Record<{
-  id: string;
-}>;
+}
 
-export type ProposalId = nat;
+export interface Investor extends User{
+  
+}
 
-export type ProposalPayload = Record<{
-  position: Record<{ lat: float32; lng: float32 }>;
-  description: string;
-}>;
 
-export type Proposal = Record<{
-  id: nat;
-  timestamp: nat64;
-  votes_yes_e8s: nat;
-  votes_no_e8s: nat;
 
-  voters: Vec<Principal>;
-  state: ProposalState;
-  proposer: Principal;
-  payload: ProposalPayload;
-}>;
-
-export type ProposalState = Variant<{
-  // A failure occurred while executing the proposal
-  Failed: text;
-  // The proposal is open for voting
-  Open: null;
-  // The proposal is currently being executed
-  Executing: null;
-  // Enough "no" votes have been cast to reject the proposal, and it will not be executed
-  Rejected: null;
-  // The proposal has been successfully executed
-  Succeeded: null;
-  // Enough "yes" votes have been cast to accept the proposal, and it will soon be executed
-  Accepted: null;
-}>;
