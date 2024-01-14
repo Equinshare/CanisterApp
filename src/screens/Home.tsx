01edf3e57df3865636c8841e373f90e64381c4d0
mburuanthony <mburuantony@outlook.com>
2024-01-14 09:38:17 +0300

    ui updates

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,28 +1,33 @@
 import { JSX } from "react";
-// import { Search } from "../components/home/Search";
-// import { CompaniesList } from "../components/home/CompaniesList";
-// import { MyInvestments } from "../components/home/MyInvestments";
-import { InvestorsList } from "../components/home/InvestorsList";
-import { CompanyAccount } from "../components/home/CompanyAccount";
+import { Search } from "../components/home/Search";
+import { CompaniesList } from "../components/home/CompaniesList";
+import { MyInvestments } from "../components/home/MyInvestments";
+import { Investmentdetail } from "../components/global/Investmentdetail";
+import { AboutCompany } from "../components/global/AboutCompany";
+// import { InvestorsList } from "../components/home/InvestorsList";
+// import { CompanyAccount } from "../components/home/CompanyAccount";
 import "../styles/screens/home.scss";
 
 export default function Home(): JSX.Element {
   return (
     <section className="main">
       <div className="left_sect">
-        <p className="invs_titles">Equinshire Investors</p>
-        <InvestorsList />
+        {/* <p className="invs_titles">Equinshire Investors</p> */}
+        {/* <InvestorsList /> */}
 
-        {/* <Search /> */}
-        {/* <p className="title">All Companies</p> */}
-        {/* <CompaniesList /> */}
+        <Search />
+        <p className="title">All Companies</p>
+        <CompaniesList />
       </div>
 
       <div className="right_sect">
-        <CompanyAccount />
-        {/* <p className="investments_title">My Investments</p> */}
-        {/* <MyInvestments /> */}
+        {/* <CompanyAccount /> */}
+        <p className="investments_title">My Investments</p>
+        <MyInvestments />
       </div>
+
+      <AboutCompany />
+      {/* <Investmentdetail /> */}
     </section>
   );
 }
